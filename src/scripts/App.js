78f45c1c4448d9eacf7b1c765bac0bdc63ab20ec
zdying <zdying@live.com>
2017-10-19 20:17:20 +0800

    update menu

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -33,15 +33,11 @@ class App extends Component {
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
             <Menu.Item key="1">
               <Icon type="user" />
-              <span>nav 1</span>
+              <span>Noah Info</span>
             </Menu.Item>
             <Menu.Item key="2">
               <Icon type="video-camera" />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span>nav 3</span>
+              <span>Env List</span>
             </Menu.Item>
           </Menu>
         </Sider>
